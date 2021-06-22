6f22efdac2c8a17f1c7856a4ce8f923613fe73d5
서창윤/서버개발팀 <changyoun.seo@nhnedu.com>
2021-06-22 18:31:48 +0900

    6. context

--- src/main/java/com/study/webclientdemo/PostClient.java
+++ src/main/java/com/study/webclientdemo/PostClient.java
@@ -9,7 +9,6 @@ package com.study.webclientdemo;/*
 
 import com.study.webclientdemo.models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -25,25 +24,25 @@ public class PostClient {
 
     @Autowired
     public PostClient() {
-        // Case 1. create
-//        this.webClient = WebClient.create(BASE_URL);
-
-        // Case 2. Builder
         this.webClient = WebClient.builder()
                                   .baseUrl(BASE_URL)
-                                  .defaultHeader(HttpHeaders.USER_AGENT, "USER_AGENT")
-                                  // .uriBuilderFactory()      // Customized UriBuilderFactory to use as a base URL.
-                                  // .defaultUriVariables()    // default values to use when expanding URI templates.
-                                  // .defaultHeader()          // Headers for every request.
-                                  // .defaultCookie()          // Cookies for every request.
-                                  // .defaultRequest()         // Consumer to customize every request.
-                                  // .filter()                 // Client filter for every request.
-                                  // .exchangeStrategies()     // HTTP message reader/writer customizations.
-                                  // .clientConnector()        // HTTP client library settings.
+                                  .filter((request, next) -> {
+                                      Mono.deferContextual(contextView -> {
+                                          // 약간 ThreadLocal 느낌
+                                          String value = contextView.get("postId");
+
+                                          System.out.println("컨텍스트: postId = " + value);
+
+                                          return Mono.empty();
+                                      });
+
+                                      return next.exchange(request);
+                                  })
                                   .build();
     }
 
     public Flux<PostModel> getPostList() {
+        // only body
         return this.webClient.get()
                              .uri("/posts")
                              .accept(MediaType.APPLICATION_JSON)
@@ -56,6 +55,19 @@ public class PostClient {
                              .uri("/posts/{postId}", postId)
                              .accept(MediaType.APPLICATION_JSON)
                              .retrieve()
-                             .bodyToMono(PostModel.class);
+                             .bodyToMono(PostModel.class)
+                             .flatMap(postModel -> {
+                                 // 결과를 체이닝 하여 추가 작업을 하는 경우
+                                 postModel.setContents(postModel.getContents() + " :: 추가 작업 후 결과");
+
+                                 return Mono.just(postModel);
+                             })
+                             .contextWrite(context -> {
+                                 // 중첩된 작업에 모두 영향을 미친다.
+                                 // 따라서 체인의 가장 마지막에 위치해야한다.
+                                 context.put("postId", postId);
+
+                                 return context;
+                             });
     }
 }
--- src/main/java/com/study/webclientdemo/TestApiController.java
+++ src/main/java/com/study/webclientdemo/TestApiController.java
@@ -8,32 +8,38 @@ package com.study.webclientdemo;/*
  */
 
 import com.study.webclientdemo.models.PostModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TestApiController {
 
-    private static final List<PostModel> POST_DATA = Arrays.asList(
-            PostModel.builder().postId(1L).title("첫글이다").contents("첫글이 내린다 샤라랄라").build(),
-            PostModel.builder().postId(2L).title("비가 많이 오네요").contents("밖에 비온다 주륵주륵").build(),
-            PostModel.builder().postId(3L).title("이건 테스트 에이피아이..").contents("테스트일뿐").build()
-    );
+    private static final List<PostModel> postList = new ArrayList<>();
+
+    static {
+        postList.add(PostModel.builder().postId(1L).title("첫글이다").contents("첫글이 내린다 샤라랄라").build());
+        postList.add(PostModel.builder().postId(2L).title("비가 많이 오네요").contents("밖에 비온다 주륵주륵").build());
+        postList.add(PostModel.builder().postId(3L).title("이건 테스트 에이피아이..").contents("테스트일뿐").build());
+    }
 
     @GetMapping("/posts")
     private Flux<PostModel> getPostList() {
-        return Flux.fromIterable(POST_DATA);
+        return Flux.fromIterable(postList);
     }
 
     @GetMapping("/posts/{postId}")
     private Mono<PostModel> getPostList(@PathVariable Long postId) {
         return getPostList().filter(postModel -> postModel.getPostId().equals(postId))
                             .next();
+    }
+
+    @PostMapping("/posts")
+    private Mono<PostModel> addPost(@RequestBody PostModel post) {
+        postList.add(post);
+        return Mono.just(post);
     }
 }
