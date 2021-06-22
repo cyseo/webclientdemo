3cd5aed986f317c42e0e85ba3c1e1b9bf078397d
서창윤/서버개발팀 <changyoun.seo@nhnedu.com>
2021-06-22 18:02:15 +0900

    2. retrieve(), Exchange

--- src/main/java/com/study/webclientdemo/PostClient.java
+++ src/main/java/com/study/webclientdemo/PostClient.java
@@ -9,8 +9,9 @@ package com.study.webclientdemo;/*
 
 import com.study.webclientdemo.models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -25,25 +26,13 @@ public class PostClient {
 
     @Autowired
     public PostClient() {
-        // Case 1. create
-//        this.webClient = WebClient.create(BASE_URL);
-
-        // Case 2. Builder
         this.webClient = WebClient.builder()
                                   .baseUrl(BASE_URL)
-                                  .defaultHeader(HttpHeaders.USER_AGENT, "USER_AGENT")
-                                  // .uriBuilderFactory()      // Customized UriBuilderFactory to use as a base URL.
-                                  // .defaultUriVariables()    // default values to use when expanding URI templates.
-                                  // .defaultHeader()          // Headers for every request.
-                                  // .defaultCookie()          // Cookies for every request.
-                                  // .defaultRequest()         // Consumer to customize every request.
-                                  // .filter()                 // Client filter for every request.
-                                  // .exchangeStrategies()     // HTTP message reader/writer customizations.
-                                  // .clientConnector()        // HTTP client library settings.
                                   .build();
     }
 
     public Flux<PostModel> getPostList() {
+        // only body
         return this.webClient.get()
                              .uri("/posts")
                              .accept(MediaType.APPLICATION_JSON)
@@ -57,5 +46,39 @@ public class PostClient {
                              .accept(MediaType.APPLICATION_JSON)
                              .retrieve()
                              .bodyToMono(PostModel.class);
+    }
+
+    public Mono<ResponseEntity<PostModel>> getPostResponseEntity(final Long postId) {
+        // get Response entity
+        return this.webClient.get()
+                             .uri("/posts/{postId}", postId)
+                             .accept(MediaType.APPLICATION_JSON)
+                             .retrieve()
+                             .toEntity(PostModel.class);
+    }
+
+    public Mono<PostModel> getPostWithHttpError(final Long postId) {
+        return this.webClient.get()
+                             .uri("/posts/{postId}", postId)
+                             .accept(MediaType.APPLICATION_JSON)
+                             .retrieve()
+//                             .onStatus(HttpStatus::is4xxClientError, response -> ...)
+//                             .onStatus(HttpStatus::is5xxServerError, response -> ...)
+                             .bodyToMono(PostModel.class);
+    }
+
+    public Mono<PostModel> getPostWithExchange(final Long postId) {
+        return this.webClient.get()
+                             .uri("/posts/{postId}", postId)
+                             .accept(MediaType.APPLICATION_JSON)
+                             .exchangeToMono(response -> {
+                                 if (response.statusCode().equals(HttpStatus.OK)) {
+                                     return response.bodyToMono(PostModel.class);
+                                 } else if (response.statusCode().is4xxClientError()) {
+                                     return Mono.empty();
+                                 } else {
+                                     return response.createException().flatMap(Mono::error);
+                                 }
+                             });
     }
 }
