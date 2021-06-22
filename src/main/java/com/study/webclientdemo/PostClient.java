86cc8cafe517249d89533d174d8ddd6d26d8ff5c
서창윤/서버개발팀 <changyoun.seo@nhnedu.com>
2021-06-22 18:17:28 +0900

    5. attributes

--- src/main/java/com/study/webclientdemo/PostClient.java
+++ src/main/java/com/study/webclientdemo/PostClient.java
@@ -9,41 +9,33 @@ package com.study.webclientdemo;/*
 
 import com.study.webclientdemo.models.PostModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Service
 public class PostClient {
 
     private static final String BASE_URL = "http://localhost:8080";
+    private static final String AUTH_REQUIRED = "AUTH_REQUIRED";
 
     private final WebClient webClient;
 
     @Autowired
     public PostClient() {
-        // Case 1. create
-//        this.webClient = WebClient.create(BASE_URL);
-
-        // Case 2. Builder
         this.webClient = WebClient.builder()
                                   .baseUrl(BASE_URL)
-                                  .defaultHeader(HttpHeaders.USER_AGENT, "USER_AGENT")
-                                  // .uriBuilderFactory()      // Customized UriBuilderFactory to use as a base URL.
-                                  // .defaultUriVariables()    // default values to use when expanding URI templates.
-                                  // .defaultHeader()          // Headers for every request.
-                                  // .defaultCookie()          // Cookies for every request.
-                                  // .defaultRequest()         // Consumer to customize every request.
-                                  // .filter()                 // Client filter for every request.
-                                  // .exchangeStrategies()     // HTTP message reader/writer customizations.
-                                  // .clientConnector()        // HTTP client library settings.
+                                  .filter(checkAuth())
                                   .build();
     }
 
     public Flux<PostModel> getPostList() {
+        // only body
         return this.webClient.get()
                              .uri("/posts")
                              .accept(MediaType.APPLICATION_JSON)
@@ -55,7 +47,22 @@ public class PostClient {
         return this.webClient.get()
                              .uri("/posts/{postId}", postId)
                              .accept(MediaType.APPLICATION_JSON)
+                             .attribute(AUTH_REQUIRED, "true")      // 현재 request 에만 영향을 미침
                              .retrieve()
                              .bodyToMono(PostModel.class);
+    }
+
+    private ExchangeFilterFunction checkAuth() {
+        return (request, next) -> {
+            final Optional<Object> isAuthRequired = request.attribute(AUTH_REQUIRED);
+
+            if (isAuthRequired.isPresent() && Boolean.parseBoolean(isAuthRequired.get().toString())) {
+                System.out.println("인증 여기서 처리");
+            } else {
+                System.out.println("운이 좋군..");
+            }
+
+            return next.exchange(request);
+        };
     }
 }
